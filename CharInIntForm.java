c6f996294f5caa22d7a811e1c1fb581e882d9774
ShamsaddinHuseynov <132603717+ShamsaddinHuseynov@users.noreply.github.com>
2023-06-04 11:35:57 +0200

    Char in Int Form

---
+++ CharInIntForm.java
@@ -0,0 +1,20 @@
+package homeworks_completed;
+public class CharInIntForm {
+    public static void main(String[] args) {
+
+        char a = '0';
+
+        char b = '0';
+
+        char c = '0';
+
+        int A = a;
+        System.out.println(A);
+
+        int B = Integer.parseInt(String.valueOf(b));
+        System.out.println(B);
+
+        int C = Character.getNumericValue(c);
+        System.out.println(C);
+    }
+}
